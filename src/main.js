51c277c7a5a055d5db9c24931a39b2daf654efdc
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2023-04-01 15:06:40 +0300

    Scroller

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import "./css/styles.css";
+import App from "./App.vue";
+import router from "./router";
+import Flicking from "../node_modules/@egjs/vue3-flicking/dist/flicking.esm";
+import "@egjs/vue3-flicking/dist/flicking.css";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.component("Flicking", Flicking);
+app.use(router);
+app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-1/3 m-auto mb-28">
+    <Flicking ref="flicking" :options="{ circular: true }">
+      <div>
+        <span class="bg-slate-200 rounded p-4 mr-2">🍉 Watermelon</span>
+        <span class="bg-slate-200 rounded p-4 mr-2">🥝 Kiwi</span>
+        <span class="bg-slate-200 rounded p-4 mr-2">🍎 Apple</span>
+        <span class="bg-slate-200 rounded p-4 mr-2">🍉 Watermelon</span>
+        <span class="bg-slate-200 rounded p-4 mr-2">🥝 Kiwi</span>
+      </div>
+      <div><span class="bg-slate-200 rounded p-4 mr-2">🍎 Apple</span></div>
+      <div><span class="bg-slate-200 rounded p-4 mr-2">🍎 Apple</span></div>
+      <div><span class="bg-slate-200 rounded p-4 mr-2">🍎 Apple</span></div>
+    </Flicking>
+
+    <div class="mt-10">
+      <Flicking
+        ref="flicking2"
+        class="flex flex-row w-1/2 m-auto h-32 rounded"
+        :options="{ circular: true }"
+      >
+        <div class="bg-orange-100 w-full">Card 1</div>
+        <div class="bg-orange-200 w-full">Card 2</div>
+        <div class="bg-orange-300 w-full">Card 3</div>
+        <div class="bg-orange-400 w-full">Card 4</div>
+        <div class="bg-orange-500 w-full">Card 5</div>
+      </Flicking>
+      <div class="mt-5">
+        <button class="bg-slate-100 px-4 py-2 rounded mr-7" @click="prev">
+          Prev
+        </button>
+        <button class="bg-slate-100 px-4 py-2 rounded" @click="next">
+          Next
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  data() {
+    return {};
+  },
+
+  methods: {
+    next() {
+      this.$refs.flicking2.next();
+    },
+    prev() {
+      this.$refs.flicking2.prev();
+    },
+  },
+};
 </script>
